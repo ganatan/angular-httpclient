28383ddc2f0deb4ec5db63ad0fc94c3a6400e916
ganatan <dannyganatan@gmail.com>
2023-02-09 11:45:32 +0100

    Angular 15.1.3 & fontawesome

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    version: 'Angular 15.1.2',
+    version: 'Angular 15.1.3',
     bootstrap: 'Bootstrap 5.2.3',
-    fontawesome: 'Font Awesome 6.2.1',
+    fontawesome: 'Font Awesome 6.3.0',
   }
 };
