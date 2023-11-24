0b35d5d594d8bc97b475ee93e0f98487936db472
ganatan <dannyganatan@gmail.com>
2023-11-24 08:38:09 +0100

    Update Angular 17.0.4 & Angular CLI 17.0.3

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })]
+};
