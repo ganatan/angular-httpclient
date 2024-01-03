0441e92eaae87f3b7ea20f5bb783736b7785226b
ganatan <dannyganatan@gmail.com>
2024-01-03 09:35:31 +0100

    Update Angular 17.0.8 & add provideHttpClient, withFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -10,7 +9,9 @@ import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(
+      withFetch(),
+    ),
     provideRouter(routes),
     provideClientHydration(),
     provideServiceWorker('ngsw-worker.js', {
