585930555ff5aaf748ceb4ad3109b463baf2d74d
ganatan <dannyganatan@gmail.com>
2025-03-11 10:29:02 +0100

    Update Angular 19.2.1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 19.0.5',
+      version: 'Angular 19.2.1',
       bootstrap: 'Bootstrap 5.3.3',
       fontawesome: 'Font Awesome 6.7.2',
     }
